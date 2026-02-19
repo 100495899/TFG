f0e261321bdc6e9bf0ef46fbf624b7269a22fb58
100495899 <100495899@alumnos.uc3m.es>
2026-02-19 09:12:45 +0100

    Prueba con dataset gutemberg_reducido --> 18 libros

--- Script_PoC/PoC.py
+++ Script_PoC/PoC.py
@@ -1,34 +1,18 @@
 import chromadb
 from chromadb.utils import embedding_functions
-import random
 import time
 import numpy as np
 import matplotlib.pyplot as plt
-import gc
 from sklearn.decomposition import PCA
+import nltk
+from nltk.corpus import gutenberg
 
 NUM_DOCS = 301
 NUM_RUIDO = NUM_DOCS - (NUM_DOCS//4)
-NUM_PRUEBAS = 50
+NUM_PRUEBAS = 300
 LONGITUD_EXACTA = 500
 TAMANO_LOTE = 5000
 NUM_DIGITOS = len(str(NUM_DOCS))
-
-vocabulario_it = ["servidor", "red", "nube", "código", "python", "base de datos", "firewall", "router", "API", "microservicios", "docker", "bug", "parche", "backend", "frontend", "latencia", "ciberseguridad", "despliegue"]
-vocabulario_finanzas = ["presupuesto", "inversión", "ROI", "fiscal", "facturación", "auditoría", "impuestos", "balance", "gastos", "ingresos", "EBITDA", "activos", "pasivos", "trimestre", "bolsa", "acciones", "tesorería"]
-vocabulario_marketing = ["campaña", "SEO", "leads", "conversión", "redes sociales", "audiencia", "branding", "publicidad", "anuncios", "engagement", "funnel", "CTR", "target", "newsletter", "patrocinio", "eventos"]
-vocabulario_rrhh = ["empleado", "despido", "contrato", "entrevista", "confidencial", "beneficios", "baja", "evaluación", "desempeño", "retención", "talento", "horario", "fichaje", "formación", "nómina", "sindicato"]
-    
-
-def generador_textos_aleatorios(vocabulario):
-  
-    num_palabras = (LONGITUD_EXACTA // 4) + 1
-
-    palabras_elegidas = random.choices(vocabulario, k=num_palabras)
-
-    texto_completo = " ".join(palabras_elegidas)
-        
-    return texto_completo[:LONGITUD_EXACTA]
 
 
 def visualizar_espacio_vectorial(collection):
@@ -45,23 +29,11 @@ def visualizar_espacio_vectorial(collection):
     pca = PCA(n_components=2)
     vectores_2d = pca.fit_transform(embeddings)
     
-    x_ruido, y_ruido = [], []
-    x_secreto, y_secreto = [], []
-    
-    for i, id_doc in enumerate(ids):
-        if id_doc in ids_secretos:
-            x_secreto.append(vectores_2d[i][0])
-            y_secreto.append(vectores_2d[i][1])
-        else:
-            x_ruido.append(vectores_2d[i][0])
-            y_ruido.append(vectores_2d[i][1])
             
     plt.figure(figsize=(10, 8))
-    plt.scatter(x_ruido, y_ruido, c='blue', label='Ruido Corporativo', alpha=0.3, s=10)
+    plt.scatter(vectores_2d[:, 0], vectores_2d[:, 1], c='blue', label='Dataset', alpha=0.3, s=10)
     
-    if x_secreto:
-        plt.scatter(x_secreto, y_secreto, c='red', label='Documento Secreto', marker='*', s=300, edgecolors='black')
-        
+ 
     plt.title("Mapa del Espacio Vectorial de ChromaDB (Reducción PCA)")
     plt.xlabel("Componente Principal 1")
     plt.ylabel("Componente Principal 2")
@@ -69,114 +41,95 @@ def visualizar_espacio_vectorial(collection):
     plt.grid(True, linestyle='--', alpha=0.5)
     plt.show()
 
+
 print("[*] Iniciando el Laboratorio de Ataque de Canal Lateral (Timing Attack)...")
 
+nltk.download('gutenberg', quiet=True)
+
 client = chromadb.EphemeralClient()
-
 emb_fn = embedding_functions.DefaultEmbeddingFunction()
-
 collection = client.create_collection(name="tfg_rag_db", embedding_function=emb_fn)
 
-print(f"[*] Generando {NUM_RUIDO} documentos de ruido de {LONGITUD_EXACTA} chars...")
+print("\n[*] Extrayendo y troceando los libros clásicos...")
+archivos_libros = gutenberg.fileids()
+textos_reales = []
 
-textos_ruido = []
-ids_ruido = []
+for archivo in archivos_libros:
+    texto_completo = gutenberg.raw(archivo)
 
-for i in range(NUM_RUIDO):
-    if i < NUM_RUIDO/3:
-        texto = generador_textos_aleatorios(vocabulario_finanzas)
+    for i in range(0, len(texto_completo), 1000):
 
-    elif NUM_RUIDO/3 < i < 2*NUM_RUIDO/3:
-        texto = generador_textos_aleatorios(vocabulario_it)
-    
-    else:
-        texto = generador_textos_aleatorios(vocabulario_marketing)
+        trozo = texto_completo[i : i + 1000]
+        if len(trozo.strip()) > 50: 
+            textos_reales.append(trozo)
 
+print(f"[*] Hemos extraído {len(textos_reales)} fragmentos.")
 
-    textos_ruido.append(texto)
+ids = [f"doc_{i:05d}" for i in range(len(textos_reales))]
 
-    ids_ruido.append(f"doc_{i:0{NUM_DIGITOS}d}")
+print("\n[*] Vectorizando textos y guardando en ChromaDB.")
+inicio_insercion = time.perf_counter()
 
+for i in range(0, len(textos_reales), TAMANO_LOTE):
 
-print(f"[*] Generando {NUM_DOCS - NUM_RUIDO} documentos secretos de {LONGITUD_EXACTA} chars...")
-
-texto_secretos = []
-ids_secretos = []
-
-for i in range(NUM_RUIDO, NUM_DOCS):
-
-    texto = generador_textos_aleatorios(vocabulario_rrhh)
-
-    texto_secretos.append(texto)
-    ids_secretos.append(f"doc_{i:0{NUM_DIGITOS}d}")
-
-
-textos_completos = textos_ruido + texto_secretos
-ids_completos = ids_ruido + ids_secretos
-
-print(f"[*] Insertando {len(textos_completos)} documentos en lotes de {TAMANO_LOTE}...")
-
-for i in range(0, len(textos_completos), TAMANO_LOTE):
-
-    lote_docs = textos_completos[i : i + TAMANO_LOTE]
-    lote_ids = ids_completos[i : i + TAMANO_LOTE]
+    fin_lote = min(i + TAMANO_LOTE, len(textos_reales))
 
     collection.add(
-        documents=lote_docs,
-        ids=lote_ids
+        documents=textos_reales[i:fin_lote],
+        ids=ids[i:fin_lote]
     )
-    print(f"    [+] Lote insertado: del {i} al {i + len(lote_docs) - 1}")
+    print(f"   [*] Insertados {fin_lote}/{len(textos_reales)} documentos...")
 
+fin_insercion = time.perf_counter()
+print(f"[*] Base de datos lista en {fin_insercion - inicio_insercion:.2f} segundos.")
 print("[+] Base de datos poblada e indexada correctamente.\n")
 
-print("[*] Visualizando base de datos vectorial")
+#print("[*] Visualizando base de datos vectorial")
 
-visualizar_espacio_vectorial(collection)
+#visualizar_espacio_vectorial(collection)
 
 print(f"[*] Iniciando bateria de ataques ({NUM_PRUEBAS} iteraciones por query)...")
 
 tiempos_acierto = []
 tiempos_fallo = []
 
-query_acierto = "confidencial despidos entrevistas"
-query_fallo = "manzana tarta comida"
+query_acierto = "¿Quién es el capitán del barco ballenero Pequod y a quién persigue?"
+query_fallo = "¿En qué casa de Hogwarts estudian Harry Potter y Ron Weasley?"
 
 vector_acierto = emb_fn([query_acierto])
 vector_fallo = emb_fn([query_fallo])
 
 collection.query(query_embeddings=vector_acierto, n_results=1)
 
-gc.disable()
 
 for i in range(NUM_PRUEBAS):
 
     inicio = time.perf_counter_ns()
     _ = collection.query(query_embeddings=vector_fallo, n_results=5)
     fin = time.perf_counter_ns()
-    tiempos_fallo.append(fin - inicio)
+    tiempos_fallo.append((fin - inicio) * 1000)
     
     inicio = time.perf_counter_ns()
     _ = collection.query(query_embeddings=vector_acierto, n_results=5)
     fin = time.perf_counter_ns()
-    tiempos_acierto.append(fin - inicio)
+    tiempos_acierto.append((fin - inicio) * 1000)
 
-gc.enable()
+    print(f"Prueba {i}/{NUM_PRUEBAS}: Fallo: {tiempos_fallo[i]} Acierto: {tiempos_acierto[i]}")
 
-tiempos_acierto_ms = np.array(tiempos_acierto) / 1_000_000
-tiempos_fallo_ms = np.array(tiempos_fallo) / 1_000_000
+print("[*] Ataque finalizado")
 
-media_acierto = np.mean(tiempos_acierto_ms)
-media_fallo = np.mean(tiempos_fallo_ms)
+media_acierto = sum(tiempos_acierto) / len(tiempos_acierto)
+media_fallo = sum(tiempos_fallo) / len(tiempos_fallo)
 
-print("\n=== RESULTADOS DEL EXPERIMENTO ===")
-print(f"Media de latencia (Documento EXISTE): {media_acierto:.4f} ms")
-print(f"Media de latencia (Documento NO EXISTE): {media_fallo:.4f} ms")
-print(f"Diferencia (Señal): {abs(media_acierto - media_fallo):.4f} ms")
+print("\n======== RESULTADOS =========")
+print(f"Latencia Acierto: {media_acierto:.4f} ms")
+print(f"Latncia Fallo: {media_fallo:.4f} ms")
+print(f"Diferencia: {abs(media_acierto - media_fallo):.4f} ms")
 
 
 plt.figure(figsize=(10, 6))
-plt.hist(tiempos_acierto_ms, bins=30, alpha=0.7, label='Documento EXISTE (Acierto)', color='red')
-plt.hist(tiempos_fallo_ms, bins=30, alpha=0.7, label='Documento NO EXISTE (Fallo)', color='blue')
+plt.hist(tiempos_acierto, bins=30, alpha=0.7, label='Acierto', color='red')
+plt.hist(tiempos_fallo, bins=30, alpha=0.7, label='Fallo', color='blue')
 plt.axvline(media_acierto, color='darkred', linestyle='dashed', linewidth=2)
 plt.axvline(media_fallo, color='darkblue', linestyle='dashed', linewidth=2)
 plt.title('Distribución de Latencias de Búsqueda Vectorial (ChromaDB HNSW)')
